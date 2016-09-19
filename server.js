051cdf21e0638f815d839b1d849b8569d444f787
krishnamoorthy <krishnamoorthy@gpslsolutions.com>
2016-09-19 21:21:45 +0530

    Added the server side coding for sub pages

--- server.js
+++ server.js
@@ -5,6 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var pages = {
+    'page1' : {
+        title : 'Page 1',
+        heading : 'About Me',
+        content : `
+            <p>paragraph 1</p>
+            <p>paragraph 2</p>`
+    },
+    'page2' : {
+        title : 'Page 2',
+        heading : 'Education',
+        content : `
+            <p>paragraph 1</p>
+            <p>paragraph 2</p>`
+    },
+    'page3' : {
+        title : 'Page 3',
+        heading : 'Career',
+        content : `
+            <p>paragraph 1</p>
+            <p>paragraph 2</p>`
+    },
+    'page4' : {
+        title : 'Page 4',
+        heading : 'Hobbies',
+        content : `
+            <p>paragraph 1</p>
+            <p>paragraph 2</p>`
+    }
+};
+
+function createPage(data){
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var myhtml = `<html><head><title>${title}</title></head>
+                <body><h3>${heading}</h3><div>${content}</div></body></html>`;
+    return myhtml;
+};
+
+app.get('/:pageid', function(req,res){
+    var pageid = req.params.pageid;
+    res.send(createPage(pages[pageid]));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
